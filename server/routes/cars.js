d8b171e9adc8db5be0398a9375f5550fb853f58f
olaludesunkanmi <olaludesunkanmi@yahoo.com>
2019-06-09 15:23:31 +0100

    user mark posted ad as sold

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -2,13 +2,15 @@ import express from 'express';
 import AdsController from '../controllers/adsContoller';
 import AdsAuth from '../middleware/adsAuth';
 
-const { validate } = AdsAuth;
-const { postAd } = AdsController;
+const { validate, validateMarkAdStatus } = AdsAuth;
+const { postAd, markPostedAd } = AdsController;
 
 
 const carRouter = express.Router();
 
 // post a car sale ad
 carRouter.post('/car', validate, postAd);
+// mark car post ad as sold
+carRouter.patch('/car/:id/status', validateMarkAdStatus, markPostedAd);
 
 export default carRouter;
