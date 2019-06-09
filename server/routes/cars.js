da1a3054a47868a7e4abb1c1f659031603fb2f53
olaludesunkanmi <olaludesunkanmi@yahoo.com>
2019-06-09 16:11:39 +0100

    user view specific car

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -2,10 +2,12 @@ import express from 'express';
 import AdsController from '../controllers/adsContoller';
 import AdsAuth from '../middleware/adsAuth';
 import CarAuth from '../middleware/carAuth';
+import CarController from '../controllers/carController';
 
 const { validate, validateMarkAdStatus } = AdsAuth;
 const { postAd, markPostedAd, updatePostedAdPrice } = AdsController;
 const { validatePostedPrice } = CarAuth;
+const { getSpecificCar } = CarController;
 
 
 const carRouter = express.Router();
@@ -16,5 +18,7 @@ carRouter.post('/car', validate, postAd);
 carRouter.patch('/car/:id/status', validateMarkAdStatus, markPostedAd);
 // update posted ad price
 carRouter.patch('/car/:id/price', validatePostedPrice, updatePostedAdPrice);
+// get a specific car
+carRouter.get('/car/:id', getSpecificCar);
 
 export default carRouter;
