a3fdb1c4fe0be97dbe3433011d3cc834e65862e5
olaludesunkanmi <olaludesunkanmi@yahoo.com>
2019-06-09 16:35:54 +0100

    view unsold cars within price range

--- server/controllers/carController.js
+++ server/controllers/carController.js
@@ -23,6 +23,28 @@ class CarController {
       data: unsoldCars,
     });
   }
+
+  static getUnsoldCarsWithinPriceRange(req, res) {
+    const Price = {
+      min_price: req.body.min_price,
+      max_price: req.body.max_price,
+    };
+    const unsoldCars = cars.filter(car => car.status === 'available');
+    const PriceRange = unsoldCars.filter(
+      p => p.price >= Price.min_price && p.price <= Price.max_price,
+    );
+    if (!PriceRange.length) {
+      res.status(404).json({
+        status: 404,
+        error: 'there are no cars within that price range not found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      data: PriceRange,
+    });
+  }
 }
 
 export default CarController;
