bbb67f3af74759096621a6b77ab64cd17c615f03
olaludesunkanmi <olaludesunkanmi@yahoo.com>
2019-06-30 05:21:37 +0100

    admin view all posted ads

--- server/controllers/adsController.js
+++ server/controllers/adsController.js
@@ -96,6 +96,30 @@ class AdsController {
     }
   }
 
+  // get all posted car ads
+  static async allPostedAd(req, res) {
+    try {
+      const findCars = 'SELECT * FROM cars';
+      const cars = await db.query(findCars);
+      if (!cars.rows[0]) {
+        res.status(404).json({
+          status: 404,
+          message: 'no car found',
+          data: [],
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        data: cars.rows,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        error: 'Server error',
+      });
+    }
+  }
+
   // update the price of the posted car ad
   static async updatePostedAdPrice(req, res) {
     try {
