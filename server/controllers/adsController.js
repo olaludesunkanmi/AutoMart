031deb3eefe6000126c7daec24d94ae010cd220f
olaludesunkanmi <olaludesunkanmi@yahoo.com>
2019-06-30 01:45:55 +0100

    mark posted ad as sold

--- server/controllers/adsController.js
+++ server/controllers/adsController.js
@@ -1,6 +1,4 @@
 /* eslint-disable camelcase */
-import ads from '../models/adsDb';
-import cars from '../models/carsDb';
 import db from '../config/index';
 
 class AdsController {
@@ -46,111 +44,56 @@ class AdsController {
     }
   }
 
-  static markPostedAd(req, res) {
-    const carId = req.params.id;
-    const carIndex = ads.findIndex(o => o.id === parseInt(carId, 10));
-    if (carIndex > -1) {
-      const originalCar = ads[carIndex];
-      if (originalCar.status !== 'available') {
-        res.status(400).json({
-          status: 400,
-          error: 'you can not change the status of this car',
+  // mark a posted car ad as sold
+  static async markPostedAd(req, res) {
+    try {
+      const findCarId = 'SELECT * FROM cars WHERE id = $1';
+      const value = parseInt(req.params.id, 10);
+      const car = await db.query(findCarId, [value]);
+      if (!car.rows[0]) {
+        res.status(404).json({
+          status: 404,
+          message: 'car post not found',
+          data: [],
         });
         return;
       }
-      const newCarStatus = {
-        id: originalCar.id,
-        email: originalCar.email,
+      if (car.rows[0].status === req.body.status) {
+        res.status(400).json({
+          status: 400,
+          error: `The car is already marked as ${car.rows[0].status}`,
+        });
+        return;
+      }
+
+      const carStatus = 'UPDATE cars SET status = $1 WHERE id = $2';
+      const values = [req.body.status, value];
+      await db.query(carStatus, values);
+      const {
+        id, owner, manufacturer, model, price, state,
+      } = car.rows[0];
+      const newCar = {
+        id,
+        owner,
         createdOn: new Date().toUTCString(),
-        manufacturer: originalCar.manufacturer,
-        model: originalCar.model,
-        price: originalCar.price,
-        state: originalCar.state,
+        manufacturer,
+        model,
+        price,
+        state,
         status: req.body.status,
       };
-      ads[carIndex] = {
-        id: originalCar.id,
-        owner: originalCar.owner,
-        createdOn: newCarStatus.createdOn,
-        manufacturer: originalCar.manufacturer,
-        model: originalCar.model,
-        price: originalCar.price,
-        state: originalCar.state,
-        status: newCarStatus.status,
-      };
-      res.status(200).json({
-        status: 200,
-        data: newCarStatus,
-      });
-      return;
-    }
-    res.status(404).json({
-      status: 404,
-      error: 'car post not found',
-    });
-  }
 
-  static updatePostedAdPrice(req, res) {
-    const carId = req.params.id;
-    const carIndex = cars.findIndex(o => o.id === parseInt(carId, 10));
-    if (carIndex > -1) {
-      const originalCar = cars[carIndex];
-      const newCar = {
-        id: originalCar.id,
-        owner: originalCar.owner,
-        createdOn: new Date().toUTCString(),
-        state: originalCar.state,
-        status: originalCar.status,
-        price: req.body.price,
-        manufacturer: originalCar.manufacturer,
-        model: originalCar.model,
-        body_type: originalCar.body_type,
-      };
-      cars[carIndex] = {
-        id: originalCar.id,
-        owner: originalCar.owner,
-        createdOn: newCar.createdOn,
-        state: originalCar.state,
-        status: originalCar.status,
-        price: newCar.price,
-        manufacturer: originalCar.manufacturer,
-        model: originalCar.model,
-        body_type: originalCar.body_type,
-      };
       res.status(200).json({
         status: 200,
         data: newCar,
       });
       return;
-    }
-    res.status(404).json({
-      status: 404,
-      error: 'car post not found',
-    });
-  }
-
-  static deletePostedAd(req, res) {
-    const post = ads.find(p => p.id === parseInt(req.params.id, 10));
-    if (!post) {
-      res.status(404).json({
-        status: 404,
-        error: 'Car Ad not found',
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        error,
       });
-      return;
     }
-    const index = ads.indexOf(post);
-    ads.splice(index, 1);
-    res.status(200).send({
-      status: 200,
-      data: 'Car Ad successfully deleted',
-    });
-  }
-
-  static allPostedAd(req, res) {
-    res.status(200).json({
-      status: 200,
-      data: ads,
-    });
   }
 }
 
